778b7339e93fa513f463a49da73302f865b1f1a8
jimijimiwalter <97167606+jimijimiwalter@users.noreply.github.com>
2023-05-21 20:06:40 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const cors = require('cors')
+
+app.use(cors(
+    {
+        allowedHeaders: ["Authorization", "Access-Control-Allow-Origin", "Content-Type", "Access-Control-Allow-Credentials"],
+        credentials: true,
+        origin: ['http://localhost:3000', 'https://localhost:3000']
+    }
+))
+
+const MAX_AGE_COOKIE = 3 * 24 * 60 * 60 * 1000 // 3d same as jwt expired time
+
+app.get('/', (req, res) => {
+    res.cookie('Authorization', "asd", { maxAge: MAX_AGE_COOKIE, httpOnly: true });
+    res.json({ "ok": 'Hello World!' })
+})
+
+app.listen(8081, () => {
+    console.log(`Example app listening on port 8081`)
+})
